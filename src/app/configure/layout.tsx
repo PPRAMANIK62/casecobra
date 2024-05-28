d2117116c10474d5273fcbf125542c7d2e5a7344
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-28 17:40:23 +0530

    steps for user done

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/components/Steps";
 import React, { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <MaxWidthWrapper className=" flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
