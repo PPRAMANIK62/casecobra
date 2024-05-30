ff520cc424c3228b0361cb650ee649b49d28e330
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-30 11:49:14 +0530

    email error fixed

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -67,7 +67,7 @@ export async function POST(req: NextRequest) {
       });
 
       await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "CaseCobra <onboarding@resend.dev>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
